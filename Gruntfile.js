1f720dee227420533421c59543f90a82343d7732
Lucas <lucas@agtlucas.com>
2013-12-28 20:55:55 -0200

    Update compass on Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -107,7 +107,7 @@ module.exports = function (grunt) {
     compass: {
       options: {
         // If you're using global Sass gems, require them here.
-        // require: ['singularity', 'jacket'],
+        require: 'susy',
         bundleExec: true,
         sassDir: '<%= yeoman.app %>/sass',
         cssDir: '.tmp/css',
