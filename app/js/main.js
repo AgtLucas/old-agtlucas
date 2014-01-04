1bd83a74cd1cc56c65a78627018e380d5075f029
Lucas <lucas@agtlucas.com>
2014-01-04 10:39:17 -0200

    Async buttons

--- app/js/main.js
+++ app/js/main.js
@@ -36,3 +36,22 @@
   };
 
 })(jQuery);
+
+// Async Share Buttons
+(function(doc, script) {
+  var js,
+      fjs = doc.getElementsByTagName(script)[0],
+      frag = doc.createDocumentFragment(),
+      add = function(url, id) {
+        if (doc.getElementById(id)) {return;}
+        js = doc.createElement(script);
+        js.src = url;
+        id && (js.id = id);
+        frag.appendChild( js );
+      };
+
+    add('http://apis.google.com/js/plusone.js');
+    add('//platform.twitter.com/widgets.js');
+
+    fjs.parentNode.insertBefore(frag, fjs);
+}(document, 'script'));
