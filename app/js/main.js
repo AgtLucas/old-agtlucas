84a8f0ce4c479c6857754b751e78be39ad9766b0
Lucas <lucas@agtlucas.com>
2014-01-13 08:57:51 -0200

    Remove pjax from main.js

--- app/js/main.js
+++ app/js/main.js
@@ -5,30 +5,30 @@
   fitVidsJs();
 
   // Check if the browser support pjax (AKA: History API)
-  if ($.support.pjax) {
-    var duration = 400;
-    $(document).pjax("a[data-pjax]", ".main", {
-      fragment: ".main",
-      duration: duration
-    }),
-    $(".main")
-      .bind("pjax:send", function() {
-        // removeTweet('.twitter-button')
-      })
-      .bind("pjax:beforeSend", function() {
-        NProgress.start(),
-        $(this).hide()
-      })
-      .bind("pjax:complete", function() {
-        $(this).fadeIn(duration),
-        fitVidsJs(),
-        // $('.gist-block').show(),
-        NProgress.done()
-      })
-      .bind("pjax:success", function() {
-        // insertTweet('.twitter-button')
-      });
-  };
+  // if ($.support.pjax) {
+  //   var duration = 400;
+  //   $(document).pjax("a[data-pjax]", ".main", {
+  //     fragment: ".main",
+  //     duration: duration
+  //   }),
+  //   $(".main")
+  //     .bind("pjax:send", function() {
+  //       // removeTweet('.twitter-button')
+  //     })
+  //     .bind("pjax:beforeSend", function() {
+  //       NProgress.start(),
+  //       $(this).hide()
+  //     })
+  //     .bind("pjax:complete", function() {
+  //       $(this).fadeIn(duration),
+  //       fitVidsJs(),
+  //       // $('.gist-block').show(),
+  //       NProgress.done()
+  //     })
+  //     .bind("pjax:success", function() {
+  //       // insertTweet('.twitter-button')
+  //     });
+  // };
 
   // Fitvids
   function fitVidsJs() {
