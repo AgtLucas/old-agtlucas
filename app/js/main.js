650ce99ff58b185823a13e5408f0b6080c29d0a7
Lucas <lucas@agtlucas.com>
2014-01-21 19:58:24 -0200

    Remove sharing

--- app/js/main.js
+++ app/js/main.js
@@ -8,21 +8,21 @@
 
 })(window, document);
 
-// Async Share Buttons
-(function(doc, script) {
-  var js,
-      fjs = doc.getElementsByTagName(script)[0],
-      frag = doc.createDocumentFragment(),
-      add = function(url, id) {
-        if (doc.getElementById(id)) {return;}
-        js = doc.createElement(script);
-        js.src = url;
-        id && (js.id = id);
-        frag.appendChild( js );
-      };
+// // Async Share Buttons
+// (function(doc, script) {
+//   var js,
+//       fjs = doc.getElementsByTagName(script)[0],
+//       frag = doc.createDocumentFragment(),
+//       add = function(url, id) {
+//         if (doc.getElementById(id)) {return;}
+//         js = doc.createElement(script);
+//         js.src = url;
+//         id && (js.id = id);
+//         frag.appendChild( js );
+//       };
 
-    add('http://apis.google.com/js/plusone.js');
-    add('//platform.twitter.com/widgets.js');
+//     add('http://apis.google.com/js/plusone.js');
+//     add('//platform.twitter.com/widgets.js');
 
-    fjs.parentNode.insertBefore(frag, fjs);
-}(document, 'script'));
+//     fjs.parentNode.insertBefore(frag, fjs);
+// }(document, 'script'));
