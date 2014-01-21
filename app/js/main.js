038f495ca49ae0234f405ddf448a94ec8eedb0b8
Lucas <lucas@agtlucas.com>
2014-01-20 22:17:14 -0200

    Adding fluidvids

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
 (function(window, document, undefined) {
   'use strict';
 
+  Fluidvids.init({
+    selector: 'iframe',
+    players: ['www.youtube.com', 'player.vimeo.com']
+  });
+
 })(window, document);
 
 // Async Share Buttons
