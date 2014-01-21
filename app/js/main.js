7028a48944f4052d5b2affc84aa0869029de82fa
Lucas <lucas@agtlucas.com>
2014-01-20 22:15:23 -0200

    Removing Fitvids and other stufss

--- app/js/main.js
+++ app/js/main.js
@@ -1,44 +1,10 @@
-(function($) {
+(function(window, document, undefined) {
   'use strict';
 
-  // Fitvids
-  fitVidsJs();
-
-  // Check if the browser support pjax (AKA: History API)
-  // if ($.support.pjax) {
-  //   var duration = 400;
-  //   $(document).pjax("a[data-pjax]", ".main", {
-  //     fragment: ".main",
-  //     duration: duration
-  //   }),
-  //   $(".main")
-  //     .bind("pjax:send", function() {
-  //       // removeTweet('.twitter-button')
-  //     })
-  //     .bind("pjax:beforeSend", function() {
-  //       NProgress.start(),
-  //       $(this).hide()
-  //     })
-  //     .bind("pjax:complete", function() {
-  //       $(this).fadeIn(duration),
-  //       fitVidsJs(),
-  //       // $('.gist-block').show(),
-  //       NProgress.done()
-  //     })
-  //     .bind("pjax:success", function() {
-  //       // insertTweet('.twitter-button')
-  //     });
-  // };
-
-  // Fitvids
-  function fitVidsJs() {
-    $('.main').fitVids();
-  };
-
-})(jQuery);
+})(window, document);
 
 // Async Share Buttons
-var asyncButtons = (function(doc, script) {
+(function(doc, script) {
   var js,
       fjs = doc.getElementsByTagName(script)[0],
       frag = doc.createDocumentFragment(),
