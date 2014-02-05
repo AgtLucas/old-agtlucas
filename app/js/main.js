f0162163e23ee5c2df7b3c9cb0fbabc29b240670
Lucas Churchill <lucas@agtlucas.com>
2014-02-04 22:11:34 -0200

    Adding comments again

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,24 @@
     players: ['www.youtube.com', 'player.vimeo.com']
   });
 
+  var showComments = document.getElementById('show-comments');
+
+  if (showComments) {
+    showComments.addEventListener('click', function() {
+      showComments.parentNode.removeChild(showComments);
+      loadDisqus();
+    });
+  }
+
+  var disqus_shortname = 'agt-lucas',
+      disqus_url = 'http://agtlucas.com/blog{{ page.url }}';
+
+  function loadDisqus() {
+    var dsq = document.createElement('script');
+    dsq.type = 'text/javascript';
+    dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  }
+
 })(window, document);
