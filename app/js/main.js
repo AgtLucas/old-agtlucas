fda59940df774420930698455ede99de2b0cb255
Lucas Churchill <lucas@agtlucas.com>
2014-03-15 04:34:07 -0300

    Comments!

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,16 @@
 /**
  * Main module
- * The module pattern: http://addyosmani.com/resources/essentialjsdesignpatterns/book/#modulepatternjavascript
+ * The main.js file
+ * Module pattern: http://addyosmani.com/resources/essentialjsdesignpatterns/book/#modulepatternjavascript
  *
  * @author Lucas <lucas@agtlucas.com>
+ * @version 0.0.0
+ * @desc
+ * @method init
+ * @method fluidVids
+ * @method showComments
+ * @method loadDisqus
+ * @retuns init();
  */
 var main = (function () {
 
@@ -10,34 +18,50 @@ var main = (function () {
 
   var main = {
 
+    /** @function
+     *  @name init */
     init: function () {
       main.fluidVids();
       main.showComments();
     },
 
+    /** @function
+     *  @name fluidVids */
     fluidVids: function () {
+      // FluidVids plugin
       Fluidvids.init({
         selector: 'iframe',
         players: ['www.youtube.com', 'player.vimeo.com']
       });
     },
 
+    /** @function
+     *  @name showComments */
     showComments: function () {
+      // Get the show-comments id reference
       var showComments = document.getElementById('show-comments');
 
+      // If there's a show-comments id on the page
       if (showComments) {
+        // Add a click event on it
         showComments.addEventListener('click', function() {
+          // Remove the show-comment div
           showComments.parentNode.removeChild(showComments);
+          // Call loadDisqus to load comments
           main.loadDisqus();
         });
       }
     },
 
+    /** @function
+     *  @name loadDisqus */
     loadDisqus: function () {
+      // Disqus configuration
       var disqus_shortname = 'agt-lucas',
           disqus_url = 'http://agtlucas.com/blog{{ page.url }}',
           dsq = document.createElement('script');
 
+      // Insert the Disqus script at the bottom of the page
       dsq.type = 'text/javascript';
       dsq.async = true;
       dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
@@ -45,6 +69,7 @@ var main = (function () {
     }
   };
 
+  /* Return main.init(); */
   return main.init();
 
 })();
