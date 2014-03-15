a36933c98c97cfcc384ecfa41437d137db510877
Lucas Churchill <lucas@agtlucas.com>
2014-03-15 04:37:56 -0300

    Just adding a space on the last function

--- app/js/main.js
+++ app/js/main.js
@@ -67,6 +67,7 @@ var main = (function () {
       dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
       (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
     }
+
   };
 
   /* Return main.init(); */
